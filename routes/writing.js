20c59e48fac65f9e83d1c7e2befb2e1a1d25c975
judemin <judemin2087@naver.com>
2023-06-18 10:56:25 +0000

    [ADD] add parsing writings logic

--- routes/writing.js
+++ routes/writing.js
@@ -10,6 +10,43 @@ router.get("/", (req, res) => {
   	res.status(200).send({message : "글 관련 로직"});
 });
 
+// writing _id로 하나의 글만을 가져오는 요청 처리
+router.get('/getWriting', async (req, res) => {
+	try{
+		const writing = await WritingModel.findOne({ _id: req.query.writingID }).lean();
+		return res.status(200).send({writing});
+	}catch (err){
+		console.log(err);
+		return res.status(500).json({
+			sucess: false,
+			message: err,
+		});
+	 }
+});
+
+// author token으로 작가의 모든 글을 가져오는 로직
+router.get('/getAuthorWritings', async (req, res) => {
+	try{
+		const authorID = await AuthorModel.getIdByToken(req.query.token);
+		// lean을 통해 객체를 document 형태가 아닌 일반 js object로 변환
+		const author = await AuthorModel.findOne({ _id: authorID }).lean();
+		const writingIDs = author.writings;
+		
+		
+		var writings = [];
+		for(var i = 0;i < writingIDs.length;i++)
+			writings.push(await WritingModel.findOne({ _id: writingIDs[i] }).lean());
+		
+		return res.status(200).send({writings});
+	}catch (err){
+		console.log(err);
+		return res.status(500).json({
+			sucess: false,
+			message: err,
+		});
+	 }
+});
+
 // 글 작성, 작가 writings token 추가 로직
 router.post('/addWriting', async (req, res) => {
 	// 두개 이상의 collection에 접근하기 때문에 transaction 사용
