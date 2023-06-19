d8d048b413662596c64fb31feb2b64f1b2b8ad77
judemin <judemin2087@naver.com>
2023-06-19 05:03:19 +0000

    [ADD] add compilation creation, writing related logic

--- routes/compilation.js
+++ routes/compilation.js
@@ -11,4 +11,64 @@ router.get("/", (req, res) => {
   	res.status(200).send({message : "모음집 관련 로직"});
 });
 
+// 모음집 만들기, author token으로 배열에 추가
+router.post('/addCompilation', async (req, res) => {
+	const session = await mongoose.startSession();
+	await session.startTransaction();
+	try {
+		const compilation = new CompilationModel(req.body);
+		const authorID = await AuthorModel.getIdByToken(compilation.authorToken);
+		await compilation.save({ session });
+				
+		const stat = await AuthorModel.findOneAndUpdate(
+			{ _id : authorID },
+			{ $push: { compilations : compilation._id } },
+			{ session: session }
+		);
+		
+		await session.commitTransaction();
+		res.status(200).json({success: true});
+	}catch (err) {
+		await session.abortTransaction();
+		res.status(500).json({success: false, err});
+		console.log(err);
+	}
+	await session.endSession();
+});
+
+// 모음집에 글 추가
+router.put('/addWriting', async (req, res) => {
+	try {
+		const compilationID = req.body.compilationID;
+				
+		const stat = await CompilationModel.findOneAndUpdate(
+			{ _id : compilationID },
+			{ $push: { writings : req.body.writingID } },
+		);
+			
+		res.status(200).json({success: true});
+	}catch (err) {
+		res.status(500).json({success: false, err});
+		console.log(err);
+	}
+});
+
+// 모음집에서 글 삭제
+router.delete('/deleteWriting', async (req, res) => {
+	try {
+		const compilationID = req.body.compilationID;
+				
+		const stat = await CompilationModel.findOneAndUpdate(
+			{ _id : compilationID },
+			{ $pull: { writings : req.body.writingID } },
+		);
+			
+		res.status(200).json({success: true});
+	}catch (err) {
+		res.status(500).json({success: false, err});
+		console.log(err);
+	}
+});
+
+
 module.exports = router;
